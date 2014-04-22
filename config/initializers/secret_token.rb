ab250a2ecca47d54756387008937143a93efc4cd
Marko Ivanek <markoivanek@gmail.com>
2014-04-22 12:02:11 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoomtownRecords::Application.config.secret_key_base = 'cfeb4a73c147b99f60880d3161145661bcdc9fca56b28e7450012bf77769299ef93423f499fa520a82b375d959ad5363aac8bf7b5458a4c7f26a63d2a4a826a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoomtownRecords::Application.config.session_store :cookie_store, key: '_doomtown-records_session'
