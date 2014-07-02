30c5f3bec639cbd461fa54d01b00ecefbc33a822
Marko Ivanek <markoivanek@gmail.com>
2014-07-02 11:42:45 +0200

    Make sure Read more link displays only if news text has been truncated

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -2,4 +2,10 @@ module NewsHelper
   def news_image(news)
     news.image_path || "news-placeholder.png"
   end
+
+  def read_more_link(news)
+    if news.body.length > 700
+      link_to "Read More", news_path(news)
+    end
+  end
 end
