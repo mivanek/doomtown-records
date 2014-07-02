7598266bfafc5880887462c1fc16c82366045d65
Marko Ivanek <markoivanek@gmail.com>
2014-07-02 11:01:45 +0200

    Fix banner links so they correctly display the current active page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def navigation_links(item)
+    if params[:controller].include? item.gsub(/_index/, '')
+      link_to '', self.send("#{item}_path"), class: "nav-link active #{item.gsub(/_index/, '')}"
+    else
+      link_to '', self.send("#{item}_path"), class: "nav-link #{item.gsub(/_index/, '')}"
+    end
+  end
 end
