f960d3ab1dfd51b8b05b0dc58fe79461905d1760
Marko Ivanek <markoivanek@gmail.com>
2014-07-14 15:03:19 +0200

    Fix styling issue, add filter for shop pages

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,4 +1,10 @@
 class ShopController < ApplicationController
   def index
+    if params[:filter]
+      @releases = Product.where("format = ?", params[:filter].downcase)
+    else
+      @releases = Product.all
+    end
+    render 'releases/index'
   end
 end
